af0c35a70696814338071c676e703f8f77c2984a
ArandaLidia <arandagomezlidia@gmail.com>
2023-12-27 10:43:31 +0100

    .

--- Calculadora.java
+++ Calculadora.java
@@ -1,45 +1,50 @@
 import java.util.Scanner;
 
 public class Calculadora {
+static int numUno, numDos;
+
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int numUno, numDos;
+        Scanner scanner = new Scanner(System.in);
+
         String op;
         System.out.println("Introduzca primer número");
-        numUno=scanner.nextInt();
+        numUno = scanner.nextInt();
         System.out.println("Introduzca segundo número");
-        numDos=scanner.nextInt();
+        numDos = scanner.nextInt();
         System.out.println("Selecciona la operación matemática: \n\n \t+ \t\t- \t\tx \t\t/");
-        op=scanner.next();
+        op = scanner.next();
 
 
-       switch (op){
+        switch (op) {
             case "+":
 
-                System.out.printf("La suma entre %d y %d da un resultado de %d",numUno,numDos, suma(numUno,numDos));
+                System.out.printf("La suma entre %d y %d da un resultado de %d", numUno, numDos, suma());
                 break;
-
+/*
             case "-":
 
-                System.out.printf("La resta entre %d y %d da un resultado de %d",numUno,numDos, resta(numUno,numDos));
+                System.out.printf("La resta entre %d y %d da un resultado de %d", numUno, numDos, resta(numUno, numDos));
                 break;
 
-           case "x":
-               System.out.printf("La multiplicación entre %d y %d da un resultado de %d",numUno,numDos, mult(numUno,numDos));
-               break;
+            case "x":
+                System.out.printf("La multiplicación entre %d y %d da un resultado de %d", numUno, numDos, mult(numUno, numDos));
+                break;
 
-           case "/":
-               System.out.printf("La división entre %d y %d da un resultado de %.2f",numUno,numDos, div(numUno,numDos));
-               break;
+            case "/":
+                System.out.printf("La división entre %d y %d da un resultado de %.2f", numUno, numDos, div(numUno, numDos));
+                break;
 
-           default:
-               System.out.println("Lo siento, esa operación aún no la tenemos contemplada");
-               break;
-
-
+            default:
+                System.out.println("Lo siento, esa operación aún no la tenemos contemplada");
+                break;
+*/
 
         }
 
-
     }
+
+    public static int suma() {
+        return numUno + numDos;
+    }
+
 }
