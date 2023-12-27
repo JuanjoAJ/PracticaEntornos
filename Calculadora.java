fe32d5ff2c09e03e2029e37578a53ae01872fd22
Juan José <148000889+JuanjoAJ@users.noreply.github.com>
2023-12-27 10:15:32 +0100

    Segunda act Juanjo

--- Calculadora.java
+++ Calculadora.java
@@ -9,18 +9,32 @@ public class Calculadora {
         numUno=scanner.nextInt();
         System.out.println("Introduzca segundo número");
         numDos=scanner.nextInt();
-        System.out.println("Selecciones la operación matemática");
+        System.out.println("Selecciona la operación matemática: \n\n \t+ \t\t- \t\tx \t\t/");
         op=scanner.next();
 
 
-        switch (op){
+       switch (op){
             case "+":
 
-                System.out.println(suma(numUno,numDos));
+                System.out.printf("La suma entre %d y %d da un resultado de %d",numUno,numDos, suma(numUno,numDos));
                 break;
 
             case "-":
-                
+
+                System.out.printf("La resta entre %d y %d da un resultado de %d",numUno,numDos, resta(numUno,numDos));
+                break;
+
+           case "x":
+               System.out.printf("La multiplicación entre %d y %d da un resultado de %d",numUno,numDos, mult(numUno,numDos));
+               break;
+
+           case "/":
+               System.out.printf("La división entre %d y %d da un resultado de %.2f",numUno,numDos, div(numUno,numDos));
+               break;
+
+           default:
+               System.out.println("Lo siento, esa operación aún no la tenemos contemplada");
+               break;
 
 
 
